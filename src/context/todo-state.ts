831af7b9d6dfb808e302c83dc1e3182d9327d33a
Manuel Entrena <manuel.entrena@gmail.com>
2022-09-25 20:27:43 +0200

    Added localstorage to persist data

--- src/context/todo-state.ts
+++ src/context/todo-state.ts
@@ -1,7 +1,11 @@
-import { atom } from "recoil";
 import { Todo } from "@/types/interface";
+import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
 
 export const todoAtom = atom<Todo[]>({
   key: "todoAtom",
   default: [],
+  effects_UNSTABLE: [persistAtom],
 });
